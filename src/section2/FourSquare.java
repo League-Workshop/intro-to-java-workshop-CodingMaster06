47a59e385ef9581b89c2b6335517a04c16664f88
CodingMaster06 <league@WTS21.attlocal.net>
2019-01-02 13:26:26 -0800

    edited code

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -5,19 +5,19 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
-
+	Robot r = new Robot();
+	
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		r.setSpeed(100);
 		// 5. Set the pen width to 5
-
+		r.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
@@ -26,8 +26,10 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		for (int i = 0; i < array.length; i++) {
+		r.move(100);
+		r.turn(90);
+		}
 	}
 
 	public static void main(String[] args) {
--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -6,24 +6,27 @@ public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
-
+    		Robot c3po = new Robot();
+c3po.miniaturize();
         // 3. Put the robot's pen down
-
+    				c3po.penDown();
 
         // 6. Make the robot move as fast as possible
-
+    		c3po.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
-
-
+    		for (int i = 0; i < 20; i++) {
         //         2. Move your robot 200 pixels
-
+    			c3po.move(30);
 
         //         4. Turn the robot 90 degrees to the right
 
-    	
+    			c3po.turn(20);
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
-   
+    			
+    }
+    		for (int i = 0; i < args.length; i++) {
+			c3po.move(5);	
+			}
     }
 }
